5f7b893bb793aea482281f1e2518c30c9e44802e
Yizhou Sun <yizhou.sun78@gmail.com>
2024-02-17 00:20:26 -0500

    Refactored LexicalAnalyzer

---
+++ src/SyntaticAnalyzer/Parser.java
@@ -0,0 +1,31 @@
+package SyntaticAnalyzer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+
+/**
+*
+* @Description COMP442 A2
+* @author Yizhou Sun 40056775
+* @date Feb 16, 2024
+*/
+
+public class Parser {
+	
+	public Parser() {
+		
+	}
+	
+	
+}
